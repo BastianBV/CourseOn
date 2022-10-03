55d3bf5680882c92ae12fffe3c66289b2103f551
BastianBV <fitzz.barker@gmail.com>
2022-10-03 16:37:22 -0500

    landing70%

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,30 @@
 import React from "react";
 import styles from "../Landing/landing.module.scss";
 import Cards from "./Cards/Cards";
+import Typical from 'react-typical'
 const Landing = () => {
 
   return (
     <>
       <div className={`${styles.wrapper}`}>
-        <span className={`${styles.typeWrap} ${styles.line}`}>Bienvenido a courseOn</span>
+        {/* <span className={`${styles.typeWrap} ${styles.line}`}>Bienvenido a courseOn</span> */}
+        <Typical
+        loop={Infinity}
+        wrapper="b"
+        steps={[
+          'Bienvenido',
+          1000,
+          'Nosotros Somos',
+          1000,
+          'Course On',
+          1000,
+          'Encuentra',
+          1000,
+          'Aprende',
+          1000,
+          'Crea...',
+          1000
+        ]}/>
       </div>
       <Cards/>
     </>
