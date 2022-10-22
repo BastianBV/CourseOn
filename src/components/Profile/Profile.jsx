3d0bb93c8dff1f8325e4dfd629fb3f83b5293275
Rods <rodolfo.perez01@iest.edu.mx>
2022-10-21 19:08:27 -0500

    get User

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SettingsProfile from "../Buttons/SettingsProfile";
 import Avatar from "@mui/material/Avatar";
 import avatar from "../../assets/images/avatar.svg";
 import Tags from "../Profile/Tags/Tags";
 import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import styles from "./profile.module.scss";
+import { getUserInfo } from "../../services/getInfo";
+
 const Profile = () => {
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getUserInfo();
+      // setUserInfo(user.data.name)
+      console.log(user);
+    };
+    fetchUser();
+  }, []);
+  const percentage = 66;
+
   return (
     <>
-      <SettingsProfile />
-      <Avatar src={avatar} sx={{ width: 90, height: 90 }} />
+      <div>
+        <Navbar />
+      </div>
+      <div className={styles.settingButton}>
+        <SettingsProfile />
+      </div>
+
+      <div>
+        <Avatar src={avatar} sx={{ width: 90, height: 90 }} />
+        <h2>{}</h2>
+      </div>
+
       <Tags />
+      <h1>Actualmente cursando</h1>
+      <div className={styles.coursePercentage}>
+        <div className={styles.graphic} style={{ width: 200, height: 200 }}>
+          <h3>Curso 1</h3>
+          <CircularProgressbar value={percentage} text={`${percentage}%`} />
+        </div>
+        <div className={styles.graphic} style={{ width: 200, height: 200 }}>
+          <h3>Curso 2</h3>
+          <CircularProgressbar value={percentage} text={`${percentage}%`} />
+        </div>
+      </div>
+      <h1>Proximos vencimientos</h1>
       <Outlet />
     </>
   );
