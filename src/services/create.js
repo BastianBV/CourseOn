8ec09e3c4c2dccb325d2ba98e89813425dbae95d
Rods <rodolfo.perez01@iest.edu.mx>
2022-10-24 18:33:39 -0500

    avances

--- src/services/create.js
+++ src/services/create.js
@@ -1,15 +1,15 @@
-export const create = async (data,token) => {
-    const response = await fetch("http:localhost:8080/courses", {
-        method:'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization:`Bearer ${token}`
-        },
-        body: JSON.stringify(data)
-    })
-    const responseData = await response.json();
-    return responseData.user;
-}
+export const create = async (data, token) => {
+  const response = await fetch("http:localhost:8080/courses", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  });
+  const responseData = await response.json();
+  return responseData.user;
+};
 
 export const updtaeCourse = async (id, data) => {
   const response = await fetch(`http://localhost:8080/courses/${id}`, {
@@ -20,4 +20,35 @@ export const updtaeCourse = async (id, data) => {
 
   const responseData = await response.json();
   return responseData.user;
+};
+
+export const getCourse = async () => {
+  const response = await fetch("http://localhost:8080/courses/");
+  const data = await response.json();
+  return data.posts;
+};
+
+export const getCourseId = async (id) => {
+  const response = await fetch(`http://localhost:8080/courses/${id}`);
+  const data = await response.json();
+  return data.posts;
+};
+
+export const getPost = async () => {
+  const response = await fetch("http://localhost:8080/courses/");
+  const data = await response.json();
+  return data.posts;
+};
+
+export const eraseCourse = async (id, token) => {
+  const response = await fetch(`http://localhost:8080/courses/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const responseData = await response.json();
+  return responseData;
 };
