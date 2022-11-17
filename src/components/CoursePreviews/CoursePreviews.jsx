407d867e547cf1eb709787b4d9aadda710c4e4f0
Rods <rodolfo.perez01@iest.edu.mx>
2022-11-16 21:51:15 -0600

    stripe

--- src/components/CoursePreviews/CoursePreviews.jsx
+++ src/components/CoursePreviews/CoursePreviews.jsx
@@ -9,12 +9,14 @@ import Title from "./Title/Title";
 import SubTitle from "./Title/SubTitle";
 import { useParams } from "react-router-dom";
 import { getCourseInfo } from "../../services/getInfo";
+import { Modal } from "@mui/material";
+import ModalSubs from "../Modal/ModalPay";
 
 const CoursePreview = () => {
   const { courseId } = useParams();
   console.log(courseId);
   const [course, setCourse] = useState({});
-
+  const [modal, setModal] = useState(false);
   useEffect(() => {
     const getCourse = async () => {
       return getCourseInfo(courseId);
@@ -63,23 +65,9 @@ const CoursePreview = () => {
           style={{ width: 400, marginBottom: 60 }}
           text="Suscribirse"
           type="submit"
+          onClick={() => setModal(true)}
         />
-        {/* <Button onClick={handleOpen}>Open modal</Button>
-<Modal
-  open={open}
-  onClose={handleClose}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
->
-  <Box sx={style}>
-    <Typography id="modal-modal-title" variant="h6" component="h2">
-      Text in a modal
-    </Typography>
-    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-    </Typography>
-  </Box>
-</Modal> */}
+        <ModalSubs modal={modal} modalClose={() => setModal(false)} />
       </div>
     </>
   );
